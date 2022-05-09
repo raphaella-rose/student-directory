ac4588138fb30ba40d6c98a5dfe4fbbe2acdd056
EC2 Default User <ec2-user@ip-172-31-8-20.ec2.internal>
2022-05-09 18:40:17 +0000

    refactored menu code

--- directory.rb
+++ directory.rb
@@ -1,24 +1,23 @@
+@students = []
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 #create an empty array
-  students = []
 #get the first name
   name = gets.chomp
 #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    if students.count == 1
+    @students << {name: name, cohort: :november}
+    if @students.count == 1
       puts "Now we have 1 student"
     else  
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     #get another name from the user
     name = gets.chomp
   end
-  #return the array of students
-  students
+
 end
 
 def print_header
@@ -26,39 +25,49 @@ def print_header
  puts "--------------" 
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  if @students.count >= 1 
+    print_header
+    print_student_list
+    print_footer
+  else
+    puts "There are no students enrolled at Villans Academy."  
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
 end
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        if students.count >= 1 
-          print_header
-          print(students)
-          print_footer(students)
-        else
-          puts "There are no students enrolled at Villans Academy."  
-        end
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
